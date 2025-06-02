3dd2a38b389a13f37c92bb92685ce84ddb5d84ca
Lam Tien Hung <lamtienhung93@gmail.com>
2025-06-03 02:19:17 +0700

    update Lab3

--- Lab3/app/src/main/java/com/example/lab3/MainActivity.java
+++ Lab3/app/src/main/java/com/example/lab3/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.lab3;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView listView;
+    String devices[] = {"Android", "IOS", "PHP", "Unity"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.device_listview);
+        CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(), devices);
+        listView.setAdapter(customAdapter);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
 }
